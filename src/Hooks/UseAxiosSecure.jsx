34f9d7435c5f0d301fb94abbaf1458c910b7574b
shohan15020 <mdshohansajjad@gmail.com>
2024-12-01 21:22:17 +0600

    update server link

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: 'https://gain-server-side-production.up.railway.app'
+  // baseURL: "http://localhost:5000",
+  baseURL: 'https://gain-server-side-production.up.railway.app'
 });
 
 const useAxiosSecure = () => {
